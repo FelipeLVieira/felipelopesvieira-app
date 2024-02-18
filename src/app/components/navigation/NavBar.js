2a68f6c6e035e97a871d2c3b9c5c7ea4e66850b4
Felipe <fullstackdev1@Felipe-MSI-Raider-GE68HX-13VG>
2024-02-17 21:27:44 -0500

    Fixing styles

--- src/app/components/navigation/NavBar.js
+++ src/app/components/navigation/NavBar.js
@@ -5,8 +5,42 @@ import Logo from "@/app/components/navigation/Logo";
 import Link from "next/link";
 import {FaGithub, FaLinkedinIn, FaYoutube} from "react-icons/fa";
 import styles from "@/app/styles/NavBar.css";
+import {useEffect} from "react";
 
 const NavBar = () => {
+    useEffect(() => {
+        // Function to load the Ko-fi script
+        const loadKofiScript = (callback) => {
+            const script = document.createElement('script');
+            script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+            script.async = true;
+            document.body.appendChild(script);
+
+            script.onload = () => {
+                if (callback) callback();
+            };
+        };
+
+        // Initialize the Ko-fi widget once the script is loaded
+        loadKofiScript(() => {
+            kofiWidgetOverlay.draw('felipelv', {
+                'type': 'floating-chat',
+                'floating-chat.donateButton.text': 'Support me',
+                'floating-chat.donateButton.background-color': '#00b9fe',
+                'floating-chat.donateButton.text-color': '#fff'
+            });
+        });
+
+        // Optional: Clean up the script when the component unmounts
+        // This might not be necessary for a widget script like Ko-fi's, but included here for completeness
+        return () => {
+            const script = document.querySelector("script[src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js']");
+            if (script) {
+                document.body.removeChild(script);
+            }
+        };
+    }, []);
+
     return (
         <div className="navbar">
             <Link href="/">
